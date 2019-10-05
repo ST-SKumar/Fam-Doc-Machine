9c0d96df050d592dd8bd5e7a2c4d3620378acf91
ST-SKumar <9041662611.SK@gmail.com>
2019-10-05 23:55:31 +0530

    Added Diseases Activity

--- app/src/main/java/com/example/famdocmachine/MainActivity.java
+++ app/src/main/java/com/example/famdocmachine/MainActivity.java
@@ -6,39 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button doc,patient;
+    private Button doctor,patient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        doc = findViewById(R.id.doctor);
-        doc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                doctor();
-            }
-        });
+
+        doctor = findViewById(R.id.doctor);
+        doctor();
+
         patient = findViewById(R.id.patient);
-        patient.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                patient();
-            }
-        });
+        patient();
     }
     public void doctor()
     {
-        Intent intent = new Intent(MainActivity.this, Doctor.class);
-        startActivity(intent);
-        Toast.makeText(this, "It Doctor !", Toast.LENGTH_LONG).show();
+        doctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Doctor.class);
+                startActivity(intent);
+//        Toast.makeText(this, "It Doctor !", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
     public void patient()
     {
-        Intent intent = new Intent(MainActivity.this,Patient.class);
-        startActivity(intent);
-        Toast.makeText(this, "It Patient !", Toast.LENGTH_LONG).show();
+
+        patient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Patient.class);
+                startActivity(intent);
+//        Toast.makeText(this, "It Patient !", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/famdocmachine/Patient.java
+++ app/src/main/java/com/example/famdocmachine/Patient.java
@@ -2,20 +2,38 @@ package com.example.famdocmachine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class Patient extends AppCompatActivity {
 
+    private Button confirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient);
+
+        confirm = findViewById(R.id.confirm);
+        confirm();
+
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<String> ad = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.age));
         ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(ad);
+    }
+    public void confirm()
+    {
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Patient.this, Diseases.class);
+                startActivity(intent);
+            }
+        });
     }
 }
